9517f7f2f29975312dc9bad0c6868df8c5cc1e68
TKYOpartner <128873901+TKYOpartner@users.noreply.github.com>
2023-10-27 14:50:16 -0400

    Built functionality to add topics

--- src/components/NewTopicForm.js
+++ src/components/NewTopicForm.js
@@ -4,7 +4,7 @@ import {useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
 import { ALL_ICONS } from "../data/icons";
-// import addTopic
+import { addTopic } from "../features/topics/topicsSlice";
 
 export default function NewTopicForm() {
   const dispatch = useDispatch();
@@ -18,7 +18,13 @@ export default function NewTopicForm() {
       return;
     }
 
-    // dispatch new topic
+    const newTopic = {
+      id: uuidv4(),
+      name,
+      icon
+    };
+
+    dispatch(addTopic(newTopic));
     navigate(ROUTES.topicsRoute());
   };
 
